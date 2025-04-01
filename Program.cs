fe480e571f0fe0371ac82321e58720099066e1d6
Jaypee Luga <code.euler@gmail.com>
2025-04-01 14:09:47 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+try
+{
+    using (var stream = new Booster.CodingTest.Library.WordStream())
+    {
+
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+            string content = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            Console.WriteLine(content);
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception Thrown:\n\nStackTrace:{ex.StackTrace}\n\n{ex.Message}");
+}
